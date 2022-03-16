879e17b2da6239e9ec28475ae649f00e8ca25828
byronekf <byronekf@gmail.com>
2022-03-16 22:31:19 +0000

    Testing how parameters are accessed in nodejs

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,16 @@
+const { urlencoded } = require("express");
 var express = require("express");
+const url = require('url');
 var router = express.Router();
 
-router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+router.get("/", function(req, res, next) {    
+    var test = "";
+    for (var propName in req.query) {
+        if (req.query.hasOwnProperty(propName)) {
+            test += propName + " " + req.query[propName] + "; ";
+        }
+    }
+    res.send("Parameters are: " + test);
 });
 
 module.exports = router;
